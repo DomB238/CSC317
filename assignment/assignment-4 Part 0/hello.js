84e41aff0b671b9ff637df5a0e25ea2fbbfcea72
DomB238 <dboykin@mail.sfsu.edu>
2025-04-16 16:34:25 -0700

    Add JavaScript assignment files

---
+++ assignment/assignment-4 Part 0/hello.js
@@ -0,0 +1,10 @@
+// Define a variable
+const language = "JavaScript";
+
+// Define a function using ES6 arrow syntax
+const createGreeting = (name) => {
+  return `Hello, ${name}!`;
+};
+
+// Output the result to the console
+console.log(createGreeting(language));
